608a7202a850c80ead1b43eeca4d7c6d0f57eeab
Phil Fenstermacher <pcfens@wm.edu>
2013-12-20 17:03:47 -0500

    Produce a slightly different error when setting up a new service - this one is more of a warning since everything still works though.

--- manifests/update.pp
+++ manifests/update.pp
@@ -1,6 +1,6 @@
 class supervisor::update {
   exec { 'supervisor::update':
-    command     => '/usr/bin/supervisorctl update',
+    command     => '/usr/bin/supervisorctl reread && /usr/bin/supervisorctl update && /usr/bin/supervisorctl reload',
     logoutput   => on_failure,
     refreshonly => true,
     require     => Service[$supervisor::params::system_service],
